17ebac7d48b31c52ff0dd3de08b6c0970744abf3
Ligiano Azzi <ldaazzi@gmail.com>
2019-04-28 15:23:50 +0100

    creating exceptions

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-$conn = new \PDO("mysql:host=mysql.ligiano.info;dbname=ligiano17","ligiano17","ligiano01");
+try {
+    $conn = new \PDO("mysql:host=mysql___.ligiano.info;dbname=ligiano17","ligiano17","ligiano01");
+} catch (\PDOException $e) {
+    echo "
+    <div> Error! </div>
+    <div> Message: ".$e->getMessage()."
+    <div> Message: ".$e->getCode();
+}
 
