89337fe896f42f18194b32577007439320479a41
Ligiano Azzi <ldaazzi@gmail.com>
2019-04-28 19:22:44 +0100

    class 3 - knowing the Exec

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
 try {
-    $conn = new \PDO("mysql:host=mysql___.ligiano.info;dbname=ligiano17","ligiano17","ligiano01");
+    $conn = new \PDO("mysql:host=mysql.ligiano.info;dbname=ligiano17","ligiano17","ligiano01");
+   
+    /* 
+    $query = "select * from products";
+    $return = $conn->exec($query);
+    print_r($return);
+    
+    echo "<hr>";
+    */
+    $query = "insert into `products` (`name`, `desc`) values ('Course PHP OO', 'One more couse for improve the knowledge')";
+    $return = $conn->exec($query);
+    print_r($return);
+
+
 } catch (\PDOException $e) {
     echo "
     <div> Error! </div>
