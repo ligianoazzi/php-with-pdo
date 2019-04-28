4adfd5158842d70bc7f2f36f6fbf104a57361692
Ligiano Azzi <ldaazzi@gmail.com>
2019-04-28 20:05:40 +0100

    class 4 - Query

--- index.php
+++ index.php
@@ -3,16 +3,27 @@
 try {
     $conn = new \PDO("mysql:host=mysql.ligiano.info;dbname=ligiano17","ligiano17","ligiano01");
    
-    /* 
+    /* Class 3 exec
     $query = "select * from products";
     $return = $conn->exec($query);
     print_r($return);
-    
-    echo "<hr>";
     */
+
+    /* Class 3 exec    
     $query = "insert into `products` (`name`, `desc`) values ('Course PHP OO', 'One more couse for improve the knowledge')";
     $return = $conn->exec($query);
     print_r($return);
+    */
+
+    /* Class 4 Query */
+    $query = "select * from products";
+
+    $stmt = $conn->query($query);
+    $list = $stmt->fetchAll();
+    
+    echo $list[0][2];
+    print_r($list);
+    
 
 
 } catch (\PDOException $e) {
